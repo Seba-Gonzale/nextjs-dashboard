449eb80a1a0ed42e6445aa1af8eaf28e475c9e25
Sebastian <gonzaleaugusto@gmail.com>
2023-12-17 17:36:12 -0300

    sip

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Ruwudu, Rubik_Bubbles, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const ruwudu = Ruwudu({ weight: '400', subsets: ['latin'] });
+export const rubik_bubbles = Rubik_Bubbles({
+  weight: '400',
+  subsets: ['latin'],
+});
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
